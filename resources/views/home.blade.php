0c4c1a46baacc3b573f538dfa002bb82ae31a944
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-08 19:25:20 +0200

    feat: card with value from the database imported using bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,24 @@
 
 @section('main-content')
 
+<div class="container">
+
+    <div class="row">
+        @forelse ($movies as $movie)
+            <div class="card col-3 p-3" >
+                <img src="https://picsum.photos/300/200" class="card-img-top" alt="film">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <p class="card-text">{{ $movie->nationality }}</p>
+                </div>
+            </div>
+        @empty
+            <h1>non è possibile visualizzare alcun film.</h1>
+        @endforelse
+    </div>
+
+</div>
+
 
 
 @endsection
